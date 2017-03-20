bc8a748241944719bb9726b166b30c842207b082
Hugang <416681736@qq.com>
2017-03-20 18:40:29 +0800

    '注册登录API及功能初步完成'

---
+++ src/components/sideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="sidebar">
+    <div class="siderbar-head">
+      <img :src="headimg" alt="侧栏头像">
+    </div>
+    <ul>
+      <li>发布文章</li>
+      <li>发布文章</li>
+      <li>发布文章</li>
+      <li>发布文章</li>
+      <li>发布文章</li>
+      <li>发布文章</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.sidebar{width: 260px; background: }
+</style>
+
+<script>
+  import headSrc from '../images/common/pic_head.jpg'
+  export default {
+    name: 'sideBar',
+    data () {
+      return {
+        headimg: headSrc
+      }
+    }
+  }
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="">
+    <xd-sidebar></xd-sidebar>
     这里是首页
   </div>
 </template>
+
+<script>
+  import sideBar from '../components/sideBar.vue'
+  export default {
+    components: {
+      'xd-sidebar': sideBar
+    }
+  }
+</script>
